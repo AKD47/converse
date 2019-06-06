5b2bccbe1ce501f83f0aa14d657db56a18c44bb0
AKD47 <van83horn@gmail.com>
2019-06-06 12:08:24 +0300

    map

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-
+document.addEventListener('mousemove', function (event) {
+    var map = document.getElementById("map");
+    var x = -(event.pageX + map.offsetLeft) / 40;
+    var y = (event.pageY + map.offsetTop) / 50;
+    map.style.backgroundPosition = x + 'px ' + y + 'px';
 });
