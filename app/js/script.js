9f6c672e92bf83f2f036d4c325d372f0c429e93e
AKD47 <van83horn@gmail.com>
2019-06-06 14:28:58 +0300

    map

--- app/js/script.js
+++ app/js/script.js
@@ -1,21 +1,41 @@
-var map = document.getElementById("map");
+var map;
 
-window.onload = function () {
-    if (window.innerWidth >= 991) {
-        map.addEventListener('mousemove', function (event) {
-            var x = -(event.pageX + map.offsetLeft) / 70;
-            var y = -(event.pageY + map.offsetTop) / 70;
-            map.style.backgroundPosition = x + 'px ' + y + 'px';
-        });
-    }
-};
-window.addEventListener('resize', function () {
-    if (window.innerWidth >= 991) {
-        map.addEventListener('mousemove', function (event) {
-            var x = -(event.pageX + map.offsetLeft) / 70;
-            var y = -(event.pageY + map.offsetTop) / 70;
-            map.style.backgroundPosition = x + 'px ' + y + 'px';
-        });
-    }
-});
-
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 55.752210, lng: 37.617928},
+        panControl: false,
+        zoomControl: false,
+        scaleControl: false,
+        zoom: 11,
+        styles: [{
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [{"color": "#444444"}]
+        }, {"featureType": "landscape", "elementType": "all", "stylers": [{"color": "#f2f2f2"}]}, {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}]
+        }, {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [{"saturation": -100}, {"lightness": 45}]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers": [{"visibility": "simplified"}]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "labels.icon",
+            "stylers": [{"visibility": "off"}]
+        }, {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}]
+        }, {"featureType": "water", "elementType": "all", "stylers": [{"color": "#46bcec"}, {"visibility": "on"}]}]
+    });
+    var marker = new google.maps.Marker({
+        position: {lat: 55.664697, lng: 37.621201},
+        map: map,
+        icon: '../img/map-g-marker.png'
+    });
+}
